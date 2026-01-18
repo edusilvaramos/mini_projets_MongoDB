3b332eacdc2f93c42f50d251a78ea64ab089b01e
Henrique <henriquepires.ge@gmail.com>
2026-01-18 18:10:37 +0100

    fix form css

--- view/templates/layout/post/createPostForm.php
+++ view/templates/layout/post/createPostForm.php
@@ -1,12 +1,12 @@
 <form action="index.php?ctrl=post&action=add" method="post">
     <label for="title">Title</label></br>
-    <input type="text" name="title" id="title" placeholder="Post title"  /></br>
+    <input class="button tertiaryButton textarea" type="text" name="title" id="title" placeholder="Post title"  /></br>
     <label for="contenu">Content</label></br>
     <!-- <input type="text" name="content" id="content" placeholder="Write here"  /></br> -->
-    <textarea name="content" id="content" rows="15" cols="100" placeholder="Write here"></textarea></br>
+    <textarea class="button tertiaryButton textarea" name="content" id="content" rows="15" placeholder="Write here"></textarea></br>
     
     <label for="category">Category</label></br>
-    <select name="category" id="category">
+    <select class="button tertiaryButton textarea" name="category" id="category">
         <option value="announcement">announcement</option>
         <option value="backend">backend</option>
         <option value="frontend">frontend</option>
@@ -15,7 +15,6 @@
         <option value="feedback">feedback</option>
     </select>
     <div class="flex flexEnd">
-        <button type="button" class="button tertiaryButton largeButton">Save Draft</button>
         <button type="submit" class="button primaryButton largeButton">Post</button>
     </div>
 </form>
